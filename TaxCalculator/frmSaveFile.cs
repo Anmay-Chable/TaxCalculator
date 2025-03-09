d27b2ffbf459866f879f6239fe1f894d0965e6b1
Angel May <Anmaychable@gmail.com>
2025-03-09 14:56:36 -0700

    Created SaveFile Class

---
+++ TaxCalculator/frmSaveFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator
+{
+    class frmSaveFile
+    {
+        public static void Save(string filePath, List<frmTaxCalculationCollection.EmployeeTaxResult> results)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    // write header
+                    writer.WriteLine("Employee ID, Taxable Income");
+
+                    // data rows
+                    foreach (var employeeTax in results)
+                    {
+                        writer.WriteLine($"{employeeTax.EmployeeID}, {employeeTax.TaxDue}");
+                    }
+                }
+                MessageBox.Show("Employee tax data saved succesfully.", "Save Successfull");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while saving the file: {ex.Message}", "Save Error");
+            }
+        }
+    }
+}
